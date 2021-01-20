066dc56bf0be04cc4639dc800c7ce3f89e7e4c2f
ЭМИИА <vstarostin@emiia.ru>
2021-01-20 22:18:20 +0300

    Create terminal1.js

---
+++ terminal1.js
@@ -0,0 +1,35 @@
+// function1([string1, string2],target id,[color1,color2])    
+ consoleText1(['Автономность 4/5 уровня', '30% экономия', 'ADAS'], 'text',['white','white','white']);
+
+function consoleText1(words, id, colors) {
+  if (colors === undefined) colors = ['transparent'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
